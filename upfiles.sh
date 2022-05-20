d8d87190dc5de92bc0f88c9f7810c797a1a3a8c0
root <root@down.subnet10071603.vcn10071603.oraclevcn.com>
2022-05-20 08:22:07 +0000

    +++ [FHD] 511SDK-043 【個撮】変態行為を楽しむ不倫旅

--- upfiles.sh
+++ upfiles.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-wget https://sukebei.nyaa.si/download/3648748.torrent
+wget https://sukebei.nyaa.si/download/3648747.torrent
 aria2c -x 64 --bt-remove-unselected-file=true --seed-time=0 --bt-tracker-connect-timeout=10 --bt-tracker-timeout=10 *.torrent
 
-rclone copy -v --checksum --transfers 12 --checkers 16 --onedrive-chunk-size 200M --exclude "*.{sh,torrent,aria2}" /home/runner/work/DrivesDown/DrivesDown/download/ "onedrive:/actionv/CHIN"
+rclone copy -v --checksum --transfers 12 --checkers 16 --onedrive-chunk-size 200M --exclude "*.{sh,torrent,aria2}" /home/runner/work/DrivesDown/DrivesDown/download/ "onedrive:/actionv/SDK"
